ff368627f3582a3f903edae5245c5bccc4d1bb44
Cbas1417 <sj153175@gmail.com>
2026-07-15 21:01:53 +0100

    Feat-Start Models Create

--- BACKEND/backend/Role/models.py
+++ BACKEND/backend/Role/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Role(models.Model):
+    id_role = models.AutoField(primary_key=True)
+    name_role = models.CharField(max_length=200)
+    description_role = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name_role
+    
+    class Meta:
+        db_table='role'
+        verbose_name='Rol'
+        verbose_name_plural='Roles'
--- BACKEND/backend/backend/urls.py
+++ BACKEND/backend/backend/urls.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('Airport', include('Airport.urls')),
+    path('Bill', include('Bill.urls')),
+    path('Booking', include('Booking.urls')),
+    path('Role', include('Role.urls')),
+    path('Service', include('Service.urls')),
+    path('User', include('User.urls')),
+    path('Vehicle', include('Vehicle.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
